c35f7477f8a6b52b6ad5646e05013576ba499ce0
Isaac <isaachanstar@gmail.com>
2013-11-14 01:02:08 +0800

    add test for catching bug2

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -6,12 +6,18 @@ import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+import math._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   property("min1") = forAll { a: Int =>
     val h = insert(a, empty)
     findMin(h) == a
+  }
+
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(b, insert(a, empty))
+    findMin(h) == min(a, b)
   }
 
   lazy val genHeap: Gen[H] = ???
