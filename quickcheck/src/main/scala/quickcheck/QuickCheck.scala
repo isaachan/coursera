e6e9f7b5860d1c8c04489e653bc6c1f66bf5dc18
Isaac <isaachanstar@gmail.com>
2013-11-14 02:16:14 +0800

    add new test and found bug 3 and 5

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -7,6 +7,7 @@ import Arbitrary._
 import Gen._
 import Prop._
 import math._
+import collection.mutable._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -23,6 +24,34 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   property("min2") = forAll { a: Int =>
     val h = insert(a, empty)
     isEmpty(deleteMin(h))
+  }
+
+  val sizedList = Gen.sized { size =>
+    Gen.listOfN(size, Gen.choose(0, 100))
+  }
+
+  property("min3") = forAll(sizedList) { list =>
+    val heap = insertAll(list, empty)
+    val sortedList = getElementsFrom(heap)
+    sortedList.sorted == sortedList
+  }
+
+  def insertAll(listToAdd: List[Int], heap: H): H = {
+    if (listToAdd.isEmpty) { heap }
+    else {
+      val h = insert(listToAdd.head, heap)
+      insertAll(listToAdd.tail, h)
+    }
+  }
+
+  def getElementsFrom(h: H): List[Int] = { getElementsFrom(h, List[Int]())  }
+
+  def getElementsFrom(h: H, list:List[Int]): List[Int] = {
+    if (isEmpty(h)) { list }
+    else {
+      val newList = list :+ (findMin(h))
+      getElementsFrom(deleteMin(h), newList)
+    }
   }
 
   lazy val genHeap: Gen[H] = ???
