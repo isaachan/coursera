90c7f40e8a1af97591a69762d39d45354f1bb335
Isaac <isaachanstar@gmail.com>
2013-11-14 01:08:22 +0800

    add new test; seems no bug found

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -20,6 +20,11 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == min(a, b)
   }
 
+  property("min2") = forAll { a: Int =>
+    val h = insert(a, empty)
+    isEmpty(deleteMin(h))
+  }
+
   lazy val genHeap: Gen[H] = ???
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
