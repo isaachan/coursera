f72a9b6ea1217b4ed3c983abec9b5f823687c401
Isaac Han <isaachanstar@gmail.com>
2013-12-01 16:19:07 +0800

    re-implemnt always/never with Promise

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -16,13 +16,17 @@ package object nodescala {
 
     /** Returns a future that is always completed with `value`.
      */
-    def always[T](value: T): Future[T] = future { value }
+    def always[T](value: T): Future[T] = {
+      val p = Promise[T]
+      p.complete(Try(value))
+      p.future
+    }
 
     /** Returns a future that is never completed.
      *
      *  This future may be useful when testing if timeout logic works correctly.
      */
-    def never[T]: Future[T] = future { throw new TimeoutException("Unkown issue") }
+    def never[T]: Future[T] = { Promise[T]().future }
 
     /** Given a list of futures `fs`, returns the future holding the list of values of all the futures from `fs`.
      *  The returned future is completed only once all of the futures in `fs` have been completed.
