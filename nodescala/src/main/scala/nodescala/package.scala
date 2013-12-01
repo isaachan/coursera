e8324a4cbd6209aeca68b002bdde48ff3857741f
Isaac Han <isaachanstar@gmail.com>
2013-12-01 16:42:08 +0800

    continueWith / continue

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -101,7 +101,13 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      val p = Promise[S]
+      f onComplete {
+        case _ => p.complete(Try(cont(f)))
+      }
+      p.future
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
@@ -109,7 +115,13 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continue[S](cont: Try[T] => S): Future[S] = ???
+    def continue[S](cont: Try[T] => S): Future[S] = {
+      val p = Promise[S]
+      f onComplete {
+        case v => p.complete(Try(cont(v)))
+      }
+      p.future
+    }
 
   }
 
